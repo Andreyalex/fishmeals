e553bf847c3f961c34fc83a7c2b69b28f0f022a0
andreyalex <andreyalek@gmail.com>
2014-03-01 14:02:45 +0000

    Updated language

--- admin/language/russian/common/footer.php
+++ admin/language/russian/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer'] = 'Данный релиз подготовлен командой <a href="http://halfhope.ru">halfhope.ru</a>';
+$_['text_footer'] = 'Powered by Opencart v2.0 (updated and fixed by Andreyalek)';
 
